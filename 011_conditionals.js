c3586f7b6e59423447e91e5a694316f431e7a7e4
James Cross <jcross2774@gmail.com>
2018-01-29 16:01:19 -0700

    Conditionals and Conditional Operators

---
+++ 011_conditionals.js
@@ -0,0 +1,12 @@
+var password = 'zxcvzxcv';
+if (password == 'asdfasdf') {
+    console.log('Yes, asdfasdf is the password');
+}
+else if (password == 'zxcvzxcv') {
+    console.log('Yes, zxcvzxcv is the password');
+}
+else {
+    console.log('Sorry, permission denied');
+}
+//# sourceMappingURL=011_conditionals.js.map 
+//# sourceMappingURL=011_conditionals.js.map
