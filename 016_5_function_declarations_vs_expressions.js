23a1c4399c5d10766443ad07c34221011533e4e5
James Cross <jcross2774@gmail.com>
2018-01-30 14:44:13 -0700

    Function declorations vs expresions

---
+++ 016_5_function_declarations_vs_expressions.js
@@ -0,0 +1,19 @@
+console.log(fullName('Jordan', 'Hudgens'));
+// console.log(otherFullName('Jordan', 'Hudgens'));
+// console.log(thirdFullName('Jordan', 'Hudgens'));
+// Function declaration
+function fullName(first, last) {
+    return first + " " + last;
+}
+// Function expression
+var otherFullName;
+otherFullName = function (first, last) {
+    return first + " " + last;
+};
+var thirdFullName = function (first, last) {
+    return first + " " + last;
+};
+console.log(fullName('Jordan', 'Hudgens'));
+console.log(otherFullName('Jordan', 'Hudgens'));
+console.log(thirdFullName('Jordan', 'Hudgens'));
+//# sourceMappingURL=016_5_function_declarations_vs_expressions.js.map
