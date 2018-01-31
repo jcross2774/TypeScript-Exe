de4b1ffcead685ac6a6249c3c908436df829af50
James Cross <jcross2774@gmail.com>
2018-01-31 13:20:31 -0700

    Objects

---
+++ 022_objects.ts
@@ -0,0 +1,13 @@
+var realUser = {
+  email: 'test@test.com',
+  firstName: 'James',
+  lastName: 'Cross',
+  sayHi() {
+    return "The force is strong with him";
+  }
+};
+
+console.log(realUser.email);
+console.log(realUser.firstName);
+console.log(realUser.lastName);
+console.log(realUser.sayHi());
